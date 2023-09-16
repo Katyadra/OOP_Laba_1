a72e228750de105d9d5da7c26e001c917ae55270
Katyadra <Katyakhorrgh@gmail.com>
2023-09-16 18:46:20 +0300

    Удаление ненужных библиотек

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using static OOP1.NumbManip;
-using System.Xml.Linq;
 
 namespace OOP1
 {
