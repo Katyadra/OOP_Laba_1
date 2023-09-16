34a9c5a3f1251e41141a6bc597381cae4f7191db
Katyadra <Katyakhorrgh@gmail.com>
2023-09-16 18:39:22 +0300

    Сама программа
    Программа использует класс NumbManip, в котором указан объект result и методы, которые берут на вход значения a и b и реализуют различные математические операции (Сложение, вычитание, умножение, деление нацело, нахождение остатка от деления) и метод, выводящий в консоль ответ.

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -1,20 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using static OOP1.NumbManip;
+using System.Xml.Linq;
+
 namespace OOP1
 {
-    public class Summ
+    public class NumbManip
     {
-       
+        public int result;
+        public int Summ(int a, int b)
+        {
+            result = a + b;
+            return result;
+        }
+        public int Minus(int a, int b)
+        {
+            result = a - b;
+            return result;
+        }
+        public int Multip(int a, int b)
+        {
+            result = a * b;
+            return result;
+        }
+        public int Del(int a, int b)
+        {
+            result = a / b;
+            return result;
+        }
+        public int Ostat(int a, int b)
+        {
+            result = a % b;
+            return result;
+        }
+        public void Output()
+        {
+            Console.WriteLine($"Результат: {result}");
+        }
     }
-    internal class Program
+
+    internal class Numbers
     {
         static void Main(string[] args)
         {
-            
-
+            int a, b, r;
+            char p;
+            NumbManip res = new NumbManip();
+            Console.WriteLine($"Введите первое число: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Введите второе число: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Выберите желаемую операцию: ");
+            p = char.Parse(Console.ReadLine());
+            if (p == '+')
+            {
+                r = res.Summ(a, b);
+                res.Output();
+            }
+            if (p == '-')
+            {
+                r = res.Minus(a, b);
+                res.Output();
+            }
+            if (p == '*')
+            {
+                r = res.Multip(a, b);
+                res.Output();
+            }
+            if (p == '/')
+            {
+                r = res.Del(a, b);
+                res.Output();
+            }
+            if (p == '%')
+            {
+                r = res.Ostat(a, b);
+                res.Output();
+            }
         }
     }
 }
+
