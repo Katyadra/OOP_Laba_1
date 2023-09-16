87985859f293fc9bf6ae4bae790859066d2584d8
Katyadra <Katyakhorrgh@gmail.com>
2023-09-16 15:09:19 +0300

    начало

---
+++ OOP1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace OOP1
+{
+    public class Summ
+    {
+       
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
